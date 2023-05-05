cdd64e281828668256ee0ee74e48d40de7adabe0
Elyas Rodrigues <elyasrz11@gmail.com>
2023-05-05 10:31:11 -0300

    Inclusão do efeito de digitação no header da página Link da Bio e a responsividade da mesma

--- src/components/bio/Header/index.jsx
+++ src/components/bio/Header/index.jsx
@@ -1,11 +1,23 @@
+import TypeIt from "typeit-react";
 import "./header.scss";
 
 function Header() {
   return (
     <header className="bioHeader">
-      <p>Designer</p>
+      <p>
+        Designer
+        <TypeIt options={{speed: 200, loop: true}}
+          getBeforeInit={(instance) => { 
+            instance
+            .type(" <span class='red-text'> bonito NÃO vende.</span>").pause(750).delete().pause(1000)
+            .type(" <span class='green-text'> estratégico VENDE!!</span>").pause(750)
+            
+          return instance;
+          }}
+        />
+      </p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
