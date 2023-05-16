12a115ff07fb466cf1230048907f7c8b70bf0d86
Elyas Rodrigues <elyasrz11@gmail.com>
2023-05-16 17:02:48 -0300

    Adição do behas

--- src/components/bio/Botoes/index.jsx
+++ src/components/bio/Botoes/index.jsx
@@ -1,5 +1,5 @@
 import port from "../../../assets/BioPage/portfolio.png";
-// import behas from '../../../assets/BioPage/behance.png'
+import behas from '../../../assets/BioPage/behas.png'
 import zap from "../../../assets/BioPage/zap.png";
 import { Link } from 'react-router-dom'
 import "./botoes.scss"
@@ -10,9 +10,9 @@ function Botoes() {
       <Link to="/" data-aos="zoom-in" data-aos-duration="1000" >
         <img src={port} alt="porfolio"  className="cardBio"/>
       </Link>
-      {/* <Link to="https://www.behance.net/elyasdsgn" target={"_blank"} data-aos="zoom-in" data-aos-duration="1500">
+      <Link to="https://www.behance.net/elyasdsgn" target={"_blank"} data-aos="zoom-in" data-aos-duration="1500">
         <img src={behas} alt="behance" className="cardBio"/>
-      </Link> */}
+      </Link>
       <Link to="https://wa.me/98988149744?text=Olá, quero fazer um orçamento!!" data-aos="zoom-in" data-aos-duration="2000">
         <img src={zap} alt="zap"  className="cardBio"/>
       </Link>
