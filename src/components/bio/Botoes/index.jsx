5d729edf018379c857a8d2a79f927f4ba503f8a2
Elyas Rodrigues <elyasrz11@gmail.com>
2023-05-16 16:44:17 -0300

    Alteração do caminho da imagem Behas

--- src/components/bio/Botoes/index.jsx
+++ src/components/bio/Botoes/index.jsx
@@ -1,5 +1,5 @@
 import port from "../../../assets/BioPage/portfolio.png";
-import behance from "../../../assets/BioPage/behance.png";
+import behas from '../../../assets/BioPage/behance.png'
 import zap from "../../../assets/BioPage/zap.png";
 import { Link } from 'react-router-dom'
 import "./botoes.scss"
@@ -11,7 +11,7 @@ function Botoes() {
         <img src={port} alt="porfolio"  className="cardBio"/>
       </Link>
       <Link to="https://www.behance.net/elyasdsgn" target={"_blank"} data-aos="zoom-in" data-aos-duration="1500">
-        <img src={behance} alt="behence" className="cardBio"/>
+        <img src={behas} alt="behance" className="cardBio"/>
       </Link>
       <Link to="https://wa.me/98988149744?text=Olá, quero fazer um orçamento!!" data-aos="zoom-in" data-aos-duration="2000">
         <img src={zap} alt="zap"  className="cardBio"/>
