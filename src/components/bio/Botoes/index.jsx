a927a6b1b41b20c45fce2da06c3de86c938d4aa0
Elyas Rodrigues <elyasrz11@gmail.com>
2023-05-30 16:06:17 -0300

    Substituição dos botões da Página de links

--- src/components/bio/Botoes/index.jsx
+++ src/components/bio/Botoes/index.jsx
@@ -1,20 +1,20 @@
-import port from "../../../assets/BioPage/portfolio.png";
-import behas from '../../../assets/BioPage/behas.png'
-import zap from "../../../assets/BioPage/zap.png";
+import port from "../../../assets/BioPage/portfolio.jpg";
+import behas from '../../../assets/BioPage/behas.jpg'
+import zap from "../../../assets/BioPage/zap.jpg";
 import { Link } from 'react-router-dom'
 import "./botoes.scss"
 
 function Botoes() {
   return (
     <section className="buttons d-flex">
-      <Link to="/" data-aos="zoom-in" data-aos-duration="1000" >
-        <img src={port} alt="porfolio"  className="cardBio"/>
+      <Link to="https://wa.me/98988149744?text=Olá, quero fazer um orçamento!!" data-aos="zoom-in" data-aos-duration="2000">
+        <img src={zap} alt="zap"  className="cardBio"/>
       </Link>
       <Link to="https://www.behance.net/elyasdsgn" target={"_blank"} data-aos="zoom-in" data-aos-duration="1500">
         <img src={behas} alt="behance" className="cardBio"/>
       </Link>
-      <Link to="https://wa.me/98988149744?text=Olá, quero fazer um orçamento!!" data-aos="zoom-in" data-aos-duration="2000">
-        <img src={zap} alt="zap"  className="cardBio"/>
+      <Link to="/" data-aos="zoom-in" data-aos-duration="1000" >
+        <img src={port} alt="porfolio"  className="cardBio"/>
       </Link>
     </section>
   );
