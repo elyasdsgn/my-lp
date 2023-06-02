1cd7e5b74889006fcbf07f7ee3abc11927274ef0
Elyas Rodrigues <elyasrz11@gmail.com>
2023-06-02 14:30:41 -0300

    Substituição da minha foto na página de Links

--- src/components/bio/Hero/index.jsx
+++ src/components/bio/Hero/index.jsx
@@ -1,10 +1,15 @@
-import eu from "../../../assets/BioPage/eu.png" 
+import eu from "../../../assets/BioPage/eu-bio.png" 
+import brilho from "../../../assets/BioPage/brilho.png" 
 import "./hero.scss"
 
 function Hero() {
   return (
     <section className="fotoHero d-flex">
+      <div className="imagem d-flex">
       <img src={eu} alt="Foto Elyas" />
+      <img src={brilho} alt="brilho" className="brilho-1" />
+      <img src={brilho} alt="brilho" className="brilho-2" />
+      </div>
       <h1>Elyas Rodrigues</h1>
       <p>Alguns links quem podem ser uteis!!</p>
     </section>
