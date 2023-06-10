098215b3ba93a4c3c5bf744c751f40cc83a678ce
Elyas Rodrigues <elyasrz11@gmail.com>
2023-06-10 12:28:14 -0300

    Alteração na Página de Links

--- src/components/bio/Hero/index.jsx
+++ src/components/bio/Hero/index.jsx
@@ -5,12 +5,14 @@ import "./hero.scss"
 function Hero() {
   return (
     <section className="fotoHero d-flex">
+      <span>Eleve o padrão da sua marca! 🔥🚀</span>
       <div className="imagem d-flex">
       <img src={eu} alt="Foto Elyas" />
       <img src={brilho} alt="brilho" className="brilho-1" />
       <img src={brilho} alt="brilho" className="brilho-2" />
       </div>
       <h1>Elyas Rodrigues</h1>
+      
       <p>Alguns links que podem ser úteis!!</p>
     </section>
   );
