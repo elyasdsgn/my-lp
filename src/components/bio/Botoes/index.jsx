5127f6cc15972a3711404c98721dcb871deef9cb
Elyas Rodrigues <elyasrz11@gmail.com>
2023-06-12 15:06:33 -0300

    adição do botão de instagram na Página de Links

--- src/components/bio/Botoes/index.jsx
+++ src/components/bio/Botoes/index.jsx
@@ -1,6 +1,7 @@
 import port from "../../../assets/BioPage/portfolio.jpg";
 import behas from '../../../assets/BioPage/behas.jpg'
 import zap from "../../../assets/BioPage/zap.jpg";
+import instagram from "../../../assets/BioPage/instagram.jpg";
 import { Link } from 'react-router-dom'
 import "./botoes.scss"
 
@@ -12,6 +13,9 @@ function Botoes() {
       </Link>
       <Link to="https://www.behance.net/elyasdsgn" target={"_blank"} data-aos="zoom-in" data-aos-duration="1500">
         <img src={behas} alt="behance" className="cardBio"/>
+      </Link>
+      <Link to="https://www.instagram.com/dsgn.elyas/" data-aos="zoom-in" data-aos-duration="1250" >
+        <img src={instagram} alt="porfolio"  className="cardBio"/>
       </Link>
       <Link to="/" data-aos="zoom-in" data-aos-duration="1000" >
         <img src={port} alt="porfolio"  className="cardBio"/>
